d78ed9c0ef34a82bf1cc5c61286b1b775fda8105
unknown <shubhamsonawane76570@gmail.com>
2023-11-09 23:00:46 +0530

    fixed key

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -24,7 +24,7 @@ const TopicsList = async () => {
   return (
     <>
       {topics.map((t) => (
-        <div className="p-4 border border-r-slate-300 my-3 flex justify-between gap-5 items-start">
+        <div key={t._id} className="p-4 border border-r-slate-300 my-3 flex justify-between gap-5 items-start">
           <div>
             <h2 className="font-bold text-2xl">{t.title}</h2>
             <div>{t.description}</div>
